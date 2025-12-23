1f73bf1bb5bc4cb9d35bd83846a3f5d3c5e5e9ee
Prateek <106758759+Prateek7071@users.noreply.github.com>
2025-12-23 12:43:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function makeid(l) {
   // write your code here
+	const numList = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	let res = ""
+	for(let i = 0;i<l;i++){
+		res+=numList.charAt((Math.floor(Math.random()*62)))
+	}
+	return res
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+//const l = prompt("Enter a number.");
+//alert(makeid(l));
